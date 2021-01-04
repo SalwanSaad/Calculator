af8da13959ff12e3945b43e96f0d17905df27554
SalwanSaad <salwansaad923@gmail.com>
2021-01-04 21:06:08 +0100

    The program has being finished

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ while enter != 'QUIT':
     select2 = select.upper()
 
     if select2 == 'SHOW':
-        print(''' 
+        print('''  
             O = To show the available mathematical operations
             Q = To quit   
             ''')
-
-    elif select2 == 'O':
+    if select2 == 'O':
         print('you can use the following operators (cos , sin , tan , + , - , * , /)')
         list2 = list.options()
+
     elif select2 == 'Q':
         print('Thank you')
         break
-
+    else:
+        print("Option not found")
